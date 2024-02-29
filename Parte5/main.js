53b1bab310258222db0dae48ca513fbc6d93b616
Gustavo Furtado <146868632+GustavoFurtad2@users.noreply.github.com>
2024-02-29 16:21:26 -0300

    Update main.js

--- Parte5/main.js
+++ Parte5/main.js
@@ -1,73 +1,37 @@
-const GAME_MENU = 0
-const GAME_PLAYING = 1
-const GAME_DEATH = 2
+/*
+let numero = 1
+numero = numero + 2  numero (1) + 2 = 3
 
-let gameState = GAME_MENU
-let font = new Font()
+numero /= 2
 
-let pad = Pads.get()
-let oldpad = pad
+  Numbers   (Numeros)
+  String    (Texto)
+  Null      (Nulo)
+  Boolean   (Boleano)
+  Undefined (Indefinido)
 
-let images = {
-    "parado": new Image("assets/1.png", RAM),
-    "andar1": new Image("assets/2.png", RAM),
-    "andar2": new Image("assets/3.png", RAM),
-    "morte": new Image("assets/4.png", RAM),
-    "cacto": new Image("assets/cacto.png", RAM),
-}
+let gameState = 1 // Essa variavel vai indicar em que estado o jogo esta
+*/
 
-class Player {
-   y = 300
+const Usuario = "Rafael" // Constantes tambem guardam valores mas não podem alterar o seu valor
+//Usuario = "Gabriel"
+console.log(Usuario)
 
-   subindo = false
-   timer = Timer.new()
-   Draw() {
-     const time = Timer.getTime(this.timer)
-     console.log(time)
-
-     if (this.subindo == false) {
-        if (Pads.check(pad, Pads.CROSS)) {
-            this.subindo = true
-        } else {
-            if (this.y < 300) {
-            this.y = this.y + 11
-            }
-        }
-     } else {
-        if (player.y < 175) {
-            this.subindo = false
-        } else {
-            this.y = this.y - 11
-        }
-     }
-
-     if (time < 500) {
-        images.andar1.draw(100, this.y)
-     } else if (time >= 500 && time <= 1000) {
-        images.andar2.draw(100, this.y)
-     } else {
-        Timer.setTime(this.timer, 0)
-        images.andar1.draw(100, this.y)
-     }
-   }
-}
-
-let player = new Player()
 while (true) {
-    Screen.clear()
-    Draw.rect(0, 0, 640, 448, Color.new(255, 255, 255))
+  Screen.clear()
+  // Sistema de cena de exemplo
+  
+  // Cena 1
+  /*
+  if (gameState == 0) { // Se a variavel gameState tiver valor de 0 então acionar o bloco de codigo
+     Draw.rect(10, 10, 100, 100, Color.new(0, 255, 0)) // Quadrado verde
+  }
+  else if (gameState == 1) { // Cena - Se a variavel gameState tiver valor de 1 então acionar o bloco de codigo
+     Draw.rect(300, 200, 100, 100, Color.new(255, 0, 0)) // Quadrado vermelho
+     Draw.rect(10, 20, 100, 100, Color.new(0, 0, 255)) // Quadrado Azul
+     Draw.rect(90, 40, 100, 100, Color.new(255, 255, 0)) // Quadrado Amarelo
 
-    oldpad = pad // Vai salvar o estado do controle do frame anterior
-    pad = Pads.get() // Ele pega o estado atual do controle
-    if (gameState == GAME_MENU) {
-        images.parado.draw(100, 300)
-        font.print(10, 50, "Aperte X para iniciar!")
-        if (Pads.check(pad, Pads.CROSS) && !Pads.check(oldpad, Pads.CROSS)) {
-            gameState = GAME_PLAYING
-        }
-    }
-    else if(gameState == GAME_PLAYING) {
-        player.Draw()
-    }
-    Screen.flip()
+  }
+  */
+   Screen.flip()
 }
