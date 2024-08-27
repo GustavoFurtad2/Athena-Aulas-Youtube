1da2638261c19002260974b7390c4e3bcaf1abf8
Gustavo <146868632+GustavoFurtad2@users.noreply.github.com>
2024-08-26 23:50:33 -0300

    Update main.js

--- Parte2/main.js
+++ Parte2/main.js
@@ -1,52 +1,12 @@
-/*
-  Para fazer uma condição você deve iniciar utilizando if
-  logo em seguida parenteses e alguns dos operadores para fazer comparar valores
-  e abrir chaves para fazer um bloco de código
+let lista = [
+  "Tomate",
+  "Batata",
+  "Alface"
+]
 
-  Operadores Aritméticos
-  +
-  -
-  /
-  *
-  ^
-  %
+lista[0] = "chocolate"
+console.log(lista[0])
 
-  Operadores relacionais
-
-  >  Maior que
-  <  Menor que
-  >= Maior ou igual a
-  <= Menor ou igual a
-  == Igual a
-  != Diferente de
-
-  Operadores lógicos
-
-  true (VERDADEIRO)
-  false (FALSO)
-  && (E)
-  || (OU)
-
-
-let soma = 2 + 2;
-let subtracao = 6 - 4;
-
-if (soma == 4 || subtracao == 4) {  Se soma for igual a 4 OU subtracao foi igual a 4 prossiga.
-  console.log("verdadeiro");
-}*/
-
-/*
-  Como criar uma função
-  Escrever function[espaço][nome da função](argumentos) {}
-  você não pode começar com numeros apenas letras e so usar letras e numeros depois
-  
-*/
-
-function soma(n1, n2) {
-  return n1 + n2 // Return retorna um valor, ou seja ele vai substuir a função pelo valor do retorno n1 + n2
-}
-
-console.log(soma(5, 5));
 while (true) {
     Screen.clear();
 
